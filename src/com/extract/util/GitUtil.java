27449006547564e1c29e5e43f09acb86d04fbd28
shan0325 <shan0325@naver.com>
2025-07-12 00:30:22 +0900

    git main branch 추가

--- src/com/extract/util/GitUtil.java
+++ src/com/extract/util/GitUtil.java
@@ -1,9 +1,8 @@
 package com.extract.util;
 
-import javax.swing.*;
-
 public class GitUtil {
     private CmdUtil cmdUtil;
+    private String originBranch;
 
     public GitUtil(CmdUtil cmdUtil) {
         this.cmdUtil = cmdUtil;
@@ -19,21 +18,25 @@ public class GitUtil {
 
     public String getCurrentBranch(String projectPath) {
         String checkBranchCommand = "git -C " + projectPath + " branch --show-current";
-        return this.cmdUtil.exec(checkBranchCommand);
+        return this.cmdUtil.exec(checkBranchCommand).trim();
     }
 
     public boolean isExistOriginMaster(String projectPath) {
         String checkOriginBranchCommand = "git -C " + projectPath + " branch -r";
         String originBranchs = this.cmdUtil.exec(checkOriginBranchCommand);
 
-        if (!originBranchs.contains("origin/master")) {
-            return false;
+        if (originBranchs.contains("origin/master")) {
+            this.originBranch = "master";
+            return true;
+        } else if (originBranchs.contains("origin/main")) {
+            this.originBranch = "main";
+            return true;
         }
-        return true;
+        return false;
     }
 
     public String getFileListByDiffOriginMaster(String projectPath) {
-        String gitDiffCommand = "git -C " + projectPath + " diff origin/master.." + getCurrentBranch(projectPath) + " --name-only";
+        String gitDiffCommand = "git -C " + projectPath + " diff origin/" + this.originBranch + ".." + getCurrentBranch(projectPath) + " --name-only";
         return this.cmdUtil.exec(gitDiffCommand);
     }
 
